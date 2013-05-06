82846ccd450999bb2ed6cd62663334a9d52eabd7
Michael Kelley <mkelley@i3webdesign.com>
2013-05-06 11:54:58 -0500

    Fixed Training Link Text

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@
     <div><a href="http://pacesetterstn.com"><img src="img/pacesetters.png"><br>Pacesetters, Inc. Home Page</a></div>`
     <div><a href="http://infoservdd.com/login"><img src="img/phs.png"><br>Practical Health Systems</a></div>
     <div><a href="http://webmd.com"><img src="img/webmd.png"><br>WebMD</a></div>
-    <div><a href="http://tndidd-pacesetters177.training.essentiallearning.com"><img src="img/cds.png"><br>College of Direct Support</a></div>
+    <div><a href="http://tndidd-pacesetters177.training.essentiallearning.com"><img src="img/cds.png"><br>Essential Learning</a></div>
     <div><a href="http://mitc.pacesetterstn.com"><img src="img/mitc.png"><br>My MITC</a></div>
     <div><a href="http://weather.com"><img src="img/weather.png"><br>Weather</a></div>
     <div><a href="http://newschannel5.com"><img src="img/newschannel5.png"><br>News Channel 5</a></div>
