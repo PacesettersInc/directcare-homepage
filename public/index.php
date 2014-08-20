1577d6e9c6d241be470ff98c59cebef4e6406e39
Michael Kelley <mkelley@i3webdesign.com>
2014-08-19 21:54:21 -0500

    Added BenefitFirst.com and removed CollegeOfDirectSupport

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@
     <div><a href="http://pacesetterstn.com"><img src="img/pacesetters.png"><br>Pacesetters, Inc. Home Page</a></div>`
     <div><a href="http://infoservdd.com/login"><img src="img/phs.png"><br>Practical Health Systems</a></div>
     <div><a href="http://webmd.com"><img src="img/webmd.png"><br>WebMD</a></div>
-    <div><a href="http://collegeofdirectsupport.com/tn"><img src="img/cds.png"><br>College of Direct Support</a></div>
+    <div><a href="http://benefitfirst.com"><img src="img/benefit_first.png"><br>Benefit First</a></div>
     <div><a href="http://tndidd-pacesetters177.training.essentiallearning.com"><img src="img/essentiallearning.png"><br>Essential Learning</a></div>
     <div><a href="http://mitc.pacesetterstn.com"><img src="img/mitc.png"><br>My MITC</a></div>
     <div><a href="http://wunderground.com"><img src="img/wunderground.png"><br>Weather</a></div>
